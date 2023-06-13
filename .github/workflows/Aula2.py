0b69ad93ba2ccfb08f8a5377fea8f101d362124b
Isabela Oliveira <belasantosoliver@gmail.com>
2023-06-13 18:20:55 -0300

    Update Aula2.py

--- .github/workflows/Aula2.py
+++ .github/workflows/Aula2.py
@@ -5,3 +5,11 @@ if trabalho_terminado == True:
 else:
     print('Não posso sair agora')
         
+estou_livre = True
+if estou_livre == True:
+    print('Ok, posso ajudar hoje sim')
+else:
+    print('Peça o meu irmão para te ajudar')
+
+
+    
